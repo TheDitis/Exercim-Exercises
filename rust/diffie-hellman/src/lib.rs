a9c3bc740cc39f4eea9130dab51c977bae237ddd
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-03-11 21:03:20 -0700

    diffie hellman cleanup

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -23,24 +23,3 @@ fn modpow(base: u64, exp: u64, modulo: u64) -> u64 {
         .to_u64()
         .unwrap_or(0)
 }
-// fn modular_exponentiation(p: u64, b_pub: u64, a: u64) -> u64 {
-//     // c = b_pub^a % p
-//     // c =     b^e % m
-//     // let mut c = 1;
-//
-//     if p == 1 {
-//         return 0
-//     }
-//     let mut r = 1;
-//     let mut b = b_pub % p;
-//     let mut e = a;
-//     let mut m = p;
-//     while e > 0 {
-//         if e % 2 == 1 {
-//             r = r * b % m;
-//         }
-//         b = b * b % m;
-//         e = e / 2;
-//     }
-//     r
-// }
