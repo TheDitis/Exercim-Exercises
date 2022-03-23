d2e036dba23de65d04d4150d6d1f230f8b548c43
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-03-22 22:59:39 -0600

    add log-levels enum exercise

---
+++ rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,36 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Debug,
+    Warning,
+    Error,
+}
+
+impl LogLevel {
+    pub fn as_str(&self) -> &str {
+        match self {
+            LogLevel::Info => "INFO",
+            LogLevel::Debug => "DEBUG",
+            LogLevel::Warning => "WARNING",
+            LogLevel::Error => "ERROR",
+        }
+    }
+}
+
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    format!("[{}]: {}", level.as_str(), message)
+}
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+pub fn debug(message: &str) -> String { log(LogLevel::Debug, message) }
+pub fn warn(message: &str) -> String { log(LogLevel::Warning, message) }
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
