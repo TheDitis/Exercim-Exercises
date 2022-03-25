f0bae35cd3b8370866f009c844f8d02ac3e36838
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-03-24 22:28:41 -0600

    low-power-embedded-game exercise

---
+++ rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,18 @@
+
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(_, item)| item)
+        .into_iter()
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        self.1.abs() + self.0.abs()
+    }
+}
