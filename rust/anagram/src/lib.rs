3d8994101293e1b9389bfcadbbf51425b29d85f4
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-03-24 21:01:14 -0600

    anagram exercise

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,21 @@
+use std::collections::{HashSet};
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut output = HashSet::new();
+    let word = word.to_lowercase();
+
+    for &other_word in possible_anagrams {
+        let word2 = other_word.to_lowercase();
+        if word != word2 && sort_string(&word) == sort_string(&word2) {
+            output.insert(other_word);
+        }
+    }
+    output
+}
+
+
+fn sort_string(s: &String) -> String {
+    let mut sorted = s.clone().chars().collect::<Vec<char>>();
+    sorted.sort_unstable();
+    sorted.iter().collect()
+}
