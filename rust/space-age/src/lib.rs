675fa87bcb86c3376906e8c3374a3912a2701e1c
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-03-24 22:16:52 -0600

    convert space-age to use custom macro

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,5 +1,3 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
 
 const SEC_PER_EARTH_YEAR: f64 = 31557600.0;
 
@@ -28,36 +26,21 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
 
-impl Planet for Mercury {
-    const RATIO: f64 = 0.2408467;
-}
-impl Planet for Venus {
-    const RATIO: f64 = 0.61519726;
-}
-impl Planet for Earth {
-    const RATIO: f64 = 1.0;
-}
-impl Planet for Mars {
-    const RATIO: f64 = 1.8808158;
-}
-impl Planet for Jupiter {
-    const RATIO: f64 = 11.862615;
-}
-impl Planet for Saturn {
-    const RATIO: f64 = 29.447498;
-}
-impl Planet for Uranus {
-    const RATIO: f64 = 84.016846;
-}
-impl Planet for Neptune {
-    const RATIO: f64 = 164.79132;
+#[macro_export(local_inner_macros)]
+macro_rules! planet {
+    ($name:ident, $ratio:expr) => {
+        pub struct $name;
+        impl Planet for $name {
+            const RATIO: f64 = $ratio;
+        }
+    };
 }
