78eaed1b641a7b2743ec3207b90e657ad8365d22
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-03-28 20:08:53 -0600

    simplify collatz-conjecture

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -3,23 +3,12 @@ pub fn collatz(n: u64) -> Option<u64> {
     let mut n = n;
     let mut i: u64 = 0;
     while n != 1 {
-        match i.checked_add(1) {
-            Some(v) => {i = v},
-            None => return None,
-        }
+        i = i.checked_add(1)?;
         let rem = n % 2;
         if rem == 0 {
             n = n / 2;
         } else {
-            match n.checked_mul(3) {
-                Some(v) => {
-                    match v.checked_add(1) {
-                        Some(v) => {n = v},
-                        None => return None,
-                    }
-                },
-                None => return None,
-            }
+            n = n.checked_mul(3)?.checked_add(1)?;
         };
     }
     Some(i)
