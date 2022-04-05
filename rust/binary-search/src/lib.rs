523e8e5e76a05fe55ae487d172e028edca4c965e
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-04-04 22:04:31 -0600

    improve binary-search exercise

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,22 +1,17 @@
+use std::cmp::Ordering;
 
-pub fn find(array: &[i32],  key: i32) -> Option<u32> {
+pub fn find(array: &[i32], key: i32) -> Option<usize> {
     let mid = array.len() / 2;
     // BASE CASE: No more sub-arrays to branch into
     if array.len() <= 1 {
         return match array.is_empty() {
             true => None,
-            false => if array[0] == key { Some(mid as u32) } else { None },
+            false => if array[0] == key { Some(mid) } else { None },
         }
     }
-    // If key is lower, check left subarray
-    if key < array[mid] {
-        let left = &array[..mid];
-        let sub_ind = find(left, key)?;
-        Some((mid - (left.len() - sub_ind as usize)) as u32)
-    }
-    // if key is greater, check right subarray
-    else {
-        let sub_ind = find(&array[mid..], key)?;
-        Some(mid as u32 + sub_ind)
+    match key.cmp(array.get(mid)?) {
+        Ordering::Equal => Some(mid),
+        Ordering::Less => find(&array[..mid], key),
+        Ordering::Greater => Some(mid + find(&array[mid..], key)?)
     }
 }
