dbfbd7d44eee31b0c3b37fba52ad417d1b905bc5
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-04-04 21:57:57 -0600

    add necleotide-count exercise

---
+++ rust/nucleotide-count/src/lib.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+
+const NUCLEOTIDES: [char; 4] = ['A', 'C', 'G', 'T'];
+
+pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
+    if !is_valid_nucleotide(&nucleotide) { return Err(nucleotide) }
+    let mut count = 0;
+    for n in dna.chars() {
+        if !is_valid_nucleotide(&n) { return Err(n) }
+        if nucleotide == n { count += 1 }
+    }
+    Ok(count)
+}
+
+pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
+    let mut output = HashMap::new();
+    if let Some(bad) = dna.chars().find(|n| { !is_valid_nucleotide(n) }) {
+        return Err(bad)
+    }
+    for n in NUCLEOTIDES {
+        match count(n, dna) {
+            Ok(count) => { output.insert(n, count); },
+            Err(_) => { return Err(n); },
+        }
+    }
+    Ok(output)
+}
+
+
+fn is_valid_nucleotide(c: &char) -> bool { NUCLEOTIDES.contains(c) }
