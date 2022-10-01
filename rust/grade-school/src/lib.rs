c6c57ddad9f07ef117ce99356d1854845cd36a87
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-09-30 23:56:24 -0600

    feat: add grade-school exercise

---
+++ rust/grade-school/src/lib.rs
@@ -0,0 +1,29 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+#[derive(Default, Clone)]
+pub struct School {
+    roster: HashMap<u32, Vec<String>>,
+}
+
+impl School {
+    pub fn new() -> School { School::default() }
+
+    pub fn add(&mut self, grade: u32, student: &str) {
+        let entry = self.roster.entry(grade).or_insert(vec![]);
+        let insert_pos = entry.iter()
+            .position(|name| { student.cmp(&name) == Ordering::Less })
+            .unwrap_or(entry.len());
+        entry.insert(insert_pos, student.to_string());
+    }
+
+    pub fn grades(&self) -> Vec<u32> {
+        let mut grades: Vec<u32> = self.roster.keys().cloned().collect();
+        grades.sort();
+        grades
+    }
+
+    pub fn grade(&self, grade: u32) -> Vec<String> {
+        self.roster.get(&grade).unwrap_or(&vec![]).clone()
+    }
+}
