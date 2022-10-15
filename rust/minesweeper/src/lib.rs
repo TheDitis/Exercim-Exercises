2d4746a78262bc59d10b11379037351ee63c0483
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-10-15 13:06:02 -0600

    feat: minesweeper performance improvements

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -22,30 +22,25 @@ impl<'a> MineBoard<'a> {
     }
 
     fn annotated(&self) -> Vec<String> {
-        let mut output: Vec<Vec<char>> = self.clone_board();
+        let mut output: Vec<Vec<char>> = self.board.iter().map(|&s| s.chars().collect()).collect();
         for (row_num, row) in self.board.iter().enumerate() {
             for (col_num, c) in row.chars().enumerate() {
                 if c.is_ascii_whitespace() {
-                    let mine_count = self.block(row_num, col_num).join("").chars()
+                    let mine_count = self.block(row_num, col_num)
                         .filter(|&c| c == '*').count();
                     if mine_count > 0 {
-                        output[row_num][col_num] = mine_count.to_string().chars().next().unwrap();
+                        output[row_num][col_num] = char::from_digit(mine_count as u32, 10).unwrap();
                     }
                 }
             }
         }
-        output.iter().map(String::from_iter).collect()
+        output.into_iter().map(String::from_iter).collect()
     }
 
     /// get a subsection of the board around a given row & column. Max size is 3x3
-    fn block(&self, row: usize, col: usize) -> Vec<String> {
+    fn block(&self, row: usize, col: usize) -> impl Iterator<Item = char> + '_ {
         let col_range = col.saturating_sub(1)..=min(self.width - 1, col + 1);
         (row.saturating_sub(1)..=min(row+1, self.height - 1))
-            .map(|r| self.board[r][col_range.clone()].to_string())
-            .collect()
-    }
-
-    fn clone_board(&self) -> Vec<Vec<char>> {
-        self.board.iter().map(|&s| s.chars().collect()).collect()
+            .flat_map(move |r| self.board[r][col_range.clone()].chars())
     }
 }
