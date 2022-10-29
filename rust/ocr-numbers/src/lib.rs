99361f089281d505d5b075c1e9925541a5519d6c
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-10-29 16:21:11 -0600

    chore: ocr-numbers flatter algorithm

--- rust/ocr-numbers/src/lib.rs
+++ rust/ocr-numbers/src/lib.rs
@@ -8,26 +8,29 @@ pub enum Error {
 
 pub fn convert(input: &str) -> Result<String, Error> {
     validity_check(input)?;
-    let split = group(input);
+    let split = group_number_strings(input);
     let parsed: Vec<String> = split.iter().map(|row| {
         row.iter().map(|n| convert_number(n.as_str())).collect()
     }).collect();
     Ok(parsed.join(","))
 }
 
-fn group(input: &str) -> Vec<Vec<String>> {
-    // Get the individual char rows without the \n or empty seperator lines
+// returns Vec<Row<NumString>> where NumString is all the chars for a given number clumped together
+fn group_number_strings(input: &str) -> Vec<Vec<String>> {
+    // Get the individual char rows without the \n or empty separator lines
     let char_rows: Vec<String> = input.split('\n').enumerate().filter_map(|(i, s)| {
         let is_separating_line = i == 3 || (i > 4 && (i - 3) % 4 == 0);
         if is_separating_line { None } else { Some(String::from(s)) }
     }).collect();
-    let num_nums_per_row: usize = char_rows.get(0).unwrap().len() / 3;
-    // Organize all chars for a given number into a flat string, and group those by row: Vec<Row<NumString>>
-    char_rows.chunks(3).map(|row| {
-        (0..num_nums_per_row *3).step_by(3).map(|i| {
-            row.iter().map(|line| &line.as_str()[i..i+3]).collect()
-        }).collect()
-    }).collect()
+    let row_width = char_rows.get(0).unwrap().len();
+    char_rows.join("").chars().enumerate().fold(vec![], |mut v, (i, c)| {
+        let row = i / (row_width * 3); // number of the current row of numbers
+        if row >= v.len() { v.push(vec![]) }  // if i is at the start of a new row of numbers, add a new row
+        let num_num = (i % row_width) / 3;  // number of the number in a given row that c belongs to to
+        if num_num >= v[row].len() { v[row].push(String::new()) } // if the number has no entry yet, make one
+        v[row][num_num].push(c); // add the char to the number string it belongs to, in the row it belongs to
+        v
+    })
 }
 
 fn convert_number(num_str: &str) -> char {
