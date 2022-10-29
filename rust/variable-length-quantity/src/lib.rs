73bf9013e97b706c8dd5c0a5c713b114b7f3946c
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-10-28 18:47:03 -0600

    chore: start vlq exercise

---
+++ rust/variable-length-quantity/src/lib.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    IncompleteNumber,
+    Overflow,
+}
+
+/// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
+pub fn to_bytes(values: &[u32]) -> Vec<u8> {
+    let output = Vec::new();
+    for value in values {
+        value
+    }
+
+    output
+}
+
+/// Given a stream of bytes, extract all numbers which are encoded in there.
+pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
+    unimplemented!("Convert the list of bytes {:?} to a list of numbers", bytes)
+}
